0535afd13ead9b08d7f943d0fb3d99b600c883c7
DESKTOP-U0L0MJS\Admin <yunhoyom@gmail.com>
2026-05-18 17:48:12 +0900

    hello world, 데이터 타입 개요

---
+++ big21/python_dev/test.py
@@ -0,0 +1,4 @@
+print('Hello World') # '' & "" : 문자열 생성 명령어
+print('안녕하세요')   # 해당 문자열 출력하는 메모리 공간을 만들어 넣는다. cpu가 실행
+                     # ctrl + F5 : 디버깅 없이 실행
+                     # 확장프로그램 : python, pylance, python debugging, Korean Language Pack
