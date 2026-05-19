f58755d228d5e740b595044c507c78cba6f750ac
DESKTOP-U0L0MJS\Admin <yunhoyom@gmail.com>
2026-05-19 10:54:44 +0900

    python, jupyter, 단축키

--- big21/python_dev/test.py
+++ big21/python_dev/test.py
@@ -2,3 +2,7 @@ print('Hello World') # '' & "" : 문자열 생성 명령어
 print('안녕하세요')   # 해당 문자열 출력하는 메모리 공간을 만들어 넣는다. cpu가 실행
                      # ctrl + F5 : 디버깅 없이 실행
                      # 확장프로그램 : python, pylance, python debugging, Korean Language Pack
+                     # test.py 에서 .py 는 모듈
+                     # 1개 모듈 안에는 200줄 넘지 않는 것이 좋음.
+                     # 200줄 넘으면 분리하는 것 고려.
+                     # jupyter 할 때는 확장자 .ipynb
