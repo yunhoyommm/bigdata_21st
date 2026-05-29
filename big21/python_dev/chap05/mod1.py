3c0220fd8fdd113e4699b970a97a8f2ddad8c55a
DESKTOP-U0L0MJS\Admin <yunhoyom@gmail.com>
2026-05-29 13:23:40 +0900

    모듈과 패키지  - 큰 단위 코드와 작은 단위 코드(모듈) 비교  - import 기능 통해 다른 모듈의 기능 쓰기  - import 방법

--- big21/python_dev/chap05/mod1.py
+++ big21/python_dev/chap05/mod1.py
@@ -1,2 +1,13 @@
 def sum(a, b):
     return a + b
+
+def safe_sum(a,b):
+    if type(a) != type(b):
+        return "더할 수 있는 값 x"
+    else:
+        return sum(a,b)
+    
+print(safe_sum('a',1))  # 에러 발생 테스트
+print(safe_sum(1,4))    # 정상 작동 테스트
+print(safe_sum(10,10.4))
+print(sum(10,10.4))     
